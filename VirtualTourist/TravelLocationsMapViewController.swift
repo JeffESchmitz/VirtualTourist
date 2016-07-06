fca9b2ab7871b8f4005b3db99b411e854c9baae1
JeffESchmitz <jeff.schmitz@realpage.com>
2016-07-06 18:26:53 -0500

    Added project layout, added first view controller.

---
+++ VirtualTourist/TravelLocationsMapViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TravelLocationsMapViewController.swift
+//  VirtualTourist
+//
+//  Created by Jeff Schmitz on 7/6/16.
+//  Copyright © 2016 Jeff Schmitz. All rights reserved.
+//
+
+import UIKit
+
+class TravelLocationsMapViewController: UIViewController {
+    
+}
