871ff4226bd9db2e4c2e80fe1941e8b0e8e632dd
JeffESchmitz <jeff.schmitz@realpage.com>
2016-07-07 21:14:13 -0500

    Adding hiding the deletePinsLabel and editButton to right navigationItem.

--- VirtualTourist/TravelLocationsMapViewController.swift
+++ VirtualTourist/TravelLocationsMapViewController.swift
@@ -12,6 +12,26 @@ import MapKit
 class TravelLocationsMapViewController: UIViewController {
     
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var deletePinsLabel: UILabel!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let rightButton = UIBarButtonItem(title: "Edit", style: UIBarButtonItemStyle.Plain, target: self, action: #selector(TravelLocationsMapViewController.showEditing(_:)))
+        navigationItem.rightBarButtonItem = rightButton
+        deletePinsLabel.hidden = true
+    }
     
+    func showEditing(sender: UIBarButtonItem)
+    {
+        setEditing(!editing, animated: true)
+        if editing == true {
+            deletePinsLabel.hidden = false
+            navigationItem.rightBarButtonItem?.title = "Done"
+        } else {
+            deletePinsLabel.hidden = true
+            navigationItem.rightBarButtonItem?.title = "Edit"
+        }
+        
+    }
 }
