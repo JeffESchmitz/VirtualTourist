c6bcbfefddbb2c5ee7970e857800a5b0536d31f1
JeffESchmitz <jeff.schmitz@realpage.com>
2016-07-10 18:56:10 -0500

    Bunch o' UI View changes in the storyboard for the PhotoCollectionView

---
+++ VirtualTourist/PhotoCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoCollectionViewCell.swift
+//  VirtualTourist
+//
+//  Created by Jeff Schmitz on 7/10/16.
+//  Copyright © 2016 Jeff Schmitz. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var cellImageView: UIImageView!
+    
+}
